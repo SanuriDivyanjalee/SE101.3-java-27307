441a65f340087530a8d7bd50d318039aba5a41c1
SanuriDivyanjalee <136305552+SanuriDivyanjalee@users.noreply.github.com>
2023-06-13 11:18:23 +0530

    Add files via upload
    Added question 3 of practical 5

---
+++ Practical_05/Q1/Q3/Prac5_3.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.prac5_3;
+
+
+public class Prac5_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Practical_05/Q1/Q3/Student.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.prac5_3;
+
+
+final class Student {
+    final int marks = 100;
+	final void display();
+   /*the outcome of running this code will be a compilation error 
+    because the 'final' keyword is used to declare the 'display()' method in the Student class
+    also the Undergraduate class is trying to extend the Student class and override the display() method
+    which means we cannot override a final method in a subclass.
+    */
+
+}
