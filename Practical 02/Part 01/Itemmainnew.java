2c7aff54d01203ea59f07a33cb06ebbd82787bc8
SanuriDivyanjalee <136305552+SanuriDivyanjalee@users.noreply.github.com>
2023-07-29 15:31:08 +0530

    Add files via upload
    added answers of java practical 02

---
+++ Practical 02/Part 01/Itemmainnew.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.itemmainnew;
+
+public class Itemmainnew {
+
+    public static void main(String[] args) {
+        Monster monster = new Monster(1, "A monster");
+        System.out.println(monster.getLocation());
+        System.out.println(monster.getDescription());
+    }
+}
---
+++ Practical 02/Part 01/Monster.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.itemmainnew;
+
+public class Monster extends Item {
+    public Monster(int location, String description) {
+super(location, description);
+
+}
+}
